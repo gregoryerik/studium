cfb57f5b6399e07e140c113da9ef1ac29318a8b1
Greg <gregoryerikadams@outlook.com>
2023-01-23 20:59:41 +0000

    Loading home page. Still under development

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,16 @@
-use actix_web::{get, Responder};
+use actix_web::{get, Responder, HttpResponse};
+use askama::Template;
 mod auth;
+mod templates;
 
 
 #[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
+async fn index() -> actix_web::Result<HttpResponse> {
+    let index_template = templates::IndexTemplate {
+        name: "Greg" // Currently used in place of auth/account system 
+    }.render().unwrap();
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(index_template))
 }
 
 #[get("/login")]
