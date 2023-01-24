007bf9d2b68b35a3bfbe1e6863e7e207eee7895d
Greg <gregoryerikadams@outlook.com>
2023-01-24 20:13:33 +0000

    Same msg as before

--- templates/static/load.js
+++ templates/static/load.js
@@ -3,9 +3,11 @@ function get_greeting() {
     let now = new Date();
     let hour = now.getHours();
     let greeting;
+    let additional;
 
     if (hour >= 22) {
-        greeting = "Good Night";
+        greeting = "All Nighter";
+        additional = "?";
     } else if (hour >= 18) {
         greeting = "Good Evening";
     } else if (hour >= 12) {
@@ -14,9 +16,15 @@ function get_greeting() {
         greeting = "Good Morning";
     }
 
-    return greeting;
+    return [greeting, additional];
 }
 
 function set_greeting_text() {
-    document.getElementById("greeting_span").textContent = get_greeting();
+    let data = get_greeting();
+    console.log(data[0], data[1]);
+    document.getElementById("greeting_span").textContent = data[0];
+
+    if (typeof data[1] != undefined) {
+        document.getElementById("additional_span").textContent = data[1];
+    }
 }
