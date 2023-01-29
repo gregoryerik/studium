13fd3ccb8e224cbaef3e54519b92711f720c197d
Greg <gregoryerikadams@outlook.com>
2023-01-29 18:44:05 +0000

    continued setup page build

---
+++ templates/static/js/setup_load.js
@@ -0,0 +1,35 @@
+function on_load() {
+    // check that the local database has been 
+    const url = location.origin + "/api/database_exists";
+    $.get(url, function(data, _status) {
+        let name = data['name'];
+        let value = data['value'];
+
+
+    })   
+}
+
+let allow_submit = [false, false];
+
+$("#name_input").keyup(function(){
+    allow_submit[0] = !validate_input(this)
+    update_button()
+})
+
+$("#email_input").keyup(function(){
+    allow_submit[1] = !validate_input(this)
+    update_button()
+})
+
+function validate_input(ix) {
+    return ix.value == ''
+}
+
+function update_button() {
+    if (allow_submit[0] == true && allow_submit[1] == true) {
+        $("#submit_form").prop("disabled", false)
+    } else {
+        $("#submit_form").prop("disabled", true)
+    }
+}
+/// on_load()
