3a2b06b885d3a97b8be5cbead94c20b43fad90d9
Greg <gregoryerikadams@outlook.com>
2023-02-07 14:00:11 +0000

    Removed experimental. Focusing on SQL

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -8,28 +8,28 @@
 
 */
 
+use crate::config;
+
+fn get_db_path() -> String {
+    let config_data = config::file::get_application_config();
+    let path = config_data.database_path.to_owned();
+    
+    path
+}
+
 
 pub mod local {
     use std::fs;
 
-    use crate::config;
-    
     #[derive(serde::Serialize)]
     pub enum ConnectionStatus {
         Success,
         Fail,
     }
-    
-    fn get_connection_path() -> String {
-        let config_data = config::file::get_application_config();
-        let path = config_data.database_path.to_owned();
-        
-        path
-    }
-    
+
     pub fn test_connection() -> ConnectionStatus{
     
-        let connection_path = get_connection_path();
+        let connection_path = super::get_db_path();
     
         match sqlite::open(connection_path) {
             Ok(_) => ConnectionStatus::Success,
@@ -41,15 +41,11 @@ pub mod local {
 
     /// Check that the SQLite file exists
     pub fn sqlite_db_exists() -> bool {
-        let path_name = get_connection_path();
+        let path_name = super::get_db_path();
         let exists = fs::metadata(path_name).is_ok();
 
         exists
     }
 
     
-}
-
-pub mod remote {
-
 }
